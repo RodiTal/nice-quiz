47a80b8b56925f5f0d085576e7a5f5c7ed38c871
RodiTal <rodik_talos@yahoo.com>
2024-11-04 23:42:34 +0200

    adding functionality to the buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/Img/logo-app.png";
+
 
 const Home =  () =>  {
     return (
@@ -11,12 +13,12 @@ const Home =  () =>  {
             <h1>Quiz App</h1>
             <div className="play-btn-container">
                 <ul>
-                    <li><a className="play-btn"  href="play">Play</a></li>
+                    <li><Link className="play-btn" to="/quiz-instructions">Play</Link></li>
                 </ul>
             </div>
             <div className="auth-btn-container">
-                <a className="auth-btns" id="login-btn" href="/login">Login</a>
-                <a className="auth-btns" id="signup-btn" href="/register">Sign Up</a>
+                <Link className="auth-btns" id="login-btn" to="/login">Login</Link>
+                <Link className="auth-btns" id="signup-btn" to="/register">Sign Up</Link>
             </div>
         </section>
       </div>
